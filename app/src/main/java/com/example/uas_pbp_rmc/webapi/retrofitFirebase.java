c59303196784cafd257558e94c0f1aba51457219
cyrillusrama <190710120@students.uajy.ac.id>
2021-12-05 23:40:32 +0700

    routing & web api

--- app/src/main/java/com/example/uas_pbp_rmc/webapi/retrofitFirebase.java
+++ app/src/main/java/com/example/uas_pbp_rmc/webapi/retrofitFirebase.java
@@ -4,7 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class retrofitFirebase {
-    public static final String BASE_URL = "https://pbp-uas-rmc-default-rtdb.asia-southeast1.firebasedatabase.app/"; // TODO : Ubah ke web api punyamu Cyrillus
+    public static final String BASE_URL = "https://pbpwebapi.000webhostapp.com/api/"; // TODO : Ubah ke web api punyamu Cyrillus
     public static Retrofit retrofit = null;
     public static Retrofit getClient() {
         if (retrofit == null) {
