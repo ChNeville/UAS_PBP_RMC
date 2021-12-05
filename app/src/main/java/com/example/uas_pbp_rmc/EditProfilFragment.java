78f41f4f43d98fadf817694f4282ce1f8ba810ed
cyrillusrama <190710120@students.uajy.ac.id>
2021-12-06 01:34:15 +0700

    update profil

---
+++ app/src/main/java/com/example/uas_pbp_rmc/EditProfilFragment.java
@@ -0,0 +1,14 @@
+package com.example.uas_pbp_rmc;
+
+import android.os.Bundle;
+
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.uas_pbp_rmc.databinding.FragmentProfilBinding;
+
+public class EditProfilFragment extends Fragment {}
